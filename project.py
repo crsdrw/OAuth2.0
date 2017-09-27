1d11cb4c64f6282a8c3a2e03a3a984184f742369
cdrew <cdrew@slb.com>
2017-09-27 16:01:21 +0100

    Login tenplate.

--- project.py
+++ project.py
@@ -159,7 +159,7 @@ def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
     login_session['state'] = state
-    return "The current session state is %s" % login_session['state']
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
